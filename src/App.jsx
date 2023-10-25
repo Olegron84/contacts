ea8dd6a10de314b5f5198ead8f3c3c12e4608f7a
Olegron84 <olegovcharov2008@gmail.com>
2023-10-25 19:15:01 +0300

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import ContactForm from "./components/ContactForm";
+import ContactsList from "./components/ContactsList";
+import Searchbar from "./components/Searchbar";
+import {
+  createContactService,
+  deleteContactService,
+  getAllContactsService,
+} from "./services/contactsService";
+// import * as contactsServices from './services/contactsService';
+
+// Синхронізувати стан contacts з localStorage по прикладу зі списку завдань
+function initContacts() {
+  const contacts = localStorage.getItem("contacts");
+  if (contacts) {
+    return JSON.parse(contacts);
+  } else {
+    return [];
+  }
+}
+function App() {
+  const [contacts, setContacts] = useState(initContacts);
+  const [query, setQuery] = useState("");
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+  function createContact(name, number) {
+    createContactService({
+      name,
+      number,
+    }).then((newContact) => {
+      setContacts((prev) => [...prev, newContact]);
+    });
+  }
+
+  useEffect(() => {
+    getAllContactsService().then((data) => {
+      setContacts(data);
+    });
+  }, []);
+
+  function deleteContact(contactId) {
+    console.log({ contactId });
+    deleteContactService(contactId).then(() => {
+      setContacts((prev) =>
+        prev.filter((contact) => {
+          return contact.id !== contactId;
+        })
+      );
+    });
+  }
+
+  const filteredContacts = contacts.filter((contact) => {
+    return contact.name.toLowerCase().includes(query.toLowerCase());
+  });
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <ContactForm onCreate={createContact} />
+      <br />
+
+      <h2>Contacts</h2>
+
+      <Searchbar query={query} setQuery={setQuery} />
+
+      <ContactsList contacts={filteredContacts} onDelete={deleteContact} />
+    </div>
+  );
+}
+
+export default App;
